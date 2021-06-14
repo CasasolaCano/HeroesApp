350751b0142c533a1a0cf7230c62de790d7db03d
CasasolaCano <casasolacontact@gmail.com>
2021-06-14 21:00:13 +0200

    showing child routes with router outlet

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -5,33 +5,43 @@ import { ListComponent } from './pages/list/list.component';
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   {
-    path: 'list',
-    component: ListComponent
-  },
-  {
-    path: 'add',
-    component: AddComponent
-  },
-  {
-    path: 'edit/:id',
-    component: AddComponent
-  },
-  {
-    path: 'search',
-    component: SearchComponent
-  },
-  {
-    path: ':id',
-    component: HeroeComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'list'
-  },
+
+    path: '',
+    component: HomeComponent,
+
+    children: [
+      
+      {
+        path: 'list',
+        component: ListComponent
+      },
+      {
+        path: 'add',
+        component: AddComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AddComponent
+      },
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: ':id',
+        component: HeroeComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
 ]
 
 @NgModule({

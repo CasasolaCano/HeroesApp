58a7e07659bcb19642497ff83c082f7ea6d44c07
CasasolaCano <casasolacontact@gmail.com>
2021-06-15 20:04:11 +0200

    Using sidenav from angular material

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddComponent } from './pages/add/add.component';
+
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+
+
 import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 
 
@@ -20,6 +27,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     HeroesRoutingModule,
     FlexLayoutModule
   ]

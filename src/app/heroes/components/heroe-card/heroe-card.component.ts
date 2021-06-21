5aee8d384b3246ed20cb0229ef9b62d1ec1b262a
CasasolaCano <casasolacontact@gmail.com>
2021-06-21 17:37:12 +0200

    Added heroe route and edit/heroe route

--- src/app/heroes/components/heroe-card/heroe-card.component.ts
+++ src/app/heroes/components/heroe-card/heroe-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { Heroe} from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-heroe-card',
